57179ce4ffa7453b9f55f722ae6c35c94bb8a6b4
sagar-pardhi <sgrpardhi007@gmail.com>
2024-09-28 12:21:46 +0530

    add: interset_is_interesting solution

--- main/main.go
+++ main/main.go
@@ -5,12 +5,26 @@ import (
 )
 
 func main() {
-	friendsList := []string{"sauce", "noodles", "béchamel", "marjoram"}
-	myList := []string{"sauce", "noodles", "meat", "tomatoes", "?"}
+	var balance float64 = 200.75
 
-	lastElem := friendsList[len(friendsList)-1]
+	switch {
+	case balance < 0:
+		fmt.Println(float32(3.213))
+	case balance >= 0 && balance < 1000:
+		fmt.Println(float32(0.5))
+	case balance >= 1000 && balance < 5000:
+		fmt.Println(float32(1.621))
+	default:
+		fmt.Println(float32(2.475))
+	}
 
-	myList[len(myList)-1] = lastElem
+	fmt.Println("Interest Rate", float64(balance*0.5*1/100))
 
-	fmt.Println(myList)
+	count := 0
+	for balance < 210 {
+		balance += 0.5 + balance
+		count++
+	}
+
+	fmt.Println(count, balance)
 }
