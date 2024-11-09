0b31656ab156c9a4918f542a33173a3f6aac3295
sagar-pardhi <sgrpardhi007@gmail.com>
2024-11-09 20:35:31 +0530

    add: airport_robot solution

--- main/main.go
+++ main/main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-	"unicode/utf8"
-)
-
-func Replace(log string, oldRune, newRune rune) string {
-	oldRuneValue := fmt.Sprintf("%c", oldRune)
-	newRuneValue := fmt.Sprintf("%c", newRune)
-
-	return strings.Replace(log, oldRuneValue, newRuneValue, 1)
-}
-
 func main() {
-	log := "please replace '👎' with '👍'"
-	res := Replace(log, '👎', '👍')
 
-	fmt.Println(res)
-
-	test := "hello❗"
-	fmt.Println(utf8.RuneCountInString(test) <= 6)
 }
