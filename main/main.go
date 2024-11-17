7b4f9fb6a54107e67d95a4a5805a6f27d5f06748
sagar-pardhi <sgrpardhi007@gmail.com>
2024-11-17 20:08:30 +0530

    add: census solution

--- main/main.go
+++ main/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type Resident struct {
+	name    string
+	age     int
+	address map[string]string
+}
+
+func main() {
+	addr := map[string]string{}
+
+	r := Resident{
+		name:    "",
+		age:     10,
+		address: addr,
+	}
+
+	fmt.Println(len(r.address))
 }
